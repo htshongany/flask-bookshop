ff848228b139e1826924ec06dfd94adf75b94c7c
htshongany <htshongany@gmail.com>
2025-02-15 19:54:09 -0800

    update -> config.py

--- config.py
+++ config.py
@@ -1,6 +1,22 @@
 import os
+from dotenv import load_dotenv
+
+# Charger les variables d'environnement à partir du fichier .env
+load_dotenv()
 
 class Config:
+    # Clé secrète pour les sessions et les formulaires
     SECRET_KEY = os.environ.get('SECRET_KEY', 'ma_cle_secrete_qui_devrait_etre_changee')
+    
+    # URL de la base de données
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Configuration pour l'envoi des emails
+    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.gmail.com')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT', 587))
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in ['true', 'on', '1']
+    MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL', 'false').lower() in ['true', 'on', '1']
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER')
